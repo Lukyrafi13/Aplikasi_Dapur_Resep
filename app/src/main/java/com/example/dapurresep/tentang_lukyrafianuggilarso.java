f39f6770983aa101abf1b7c9a317a240672e92a4
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2020-04-23 18:00:15 +0700

    Pembuatan projek baru dan penambahan activity

---
+++ app/src/main/java/com/example/dapurresep/tentang_lukyrafianuggilarso.java
@@ -0,0 +1,14 @@
+package com.example.dapurresep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tentang_lukyrafianuggilarso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_lukyrafianuggilarso);
+    }
+}
